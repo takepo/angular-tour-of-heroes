1e691ed42fa7616882f43ca490a4120afdc98112
TakeshimaNahoko <takeshima.nahoko@lancers.co.jp>
2019-11-26 13:55:52 +0900

    feat: ヒーローの選択ができるように

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,15 +8,20 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
+  constructor() { }
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
+
   heroes = HEROES;
 
-  constructor() { }
+  selectedHero: Hero;
 
   ngOnInit() {
   }
-
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
